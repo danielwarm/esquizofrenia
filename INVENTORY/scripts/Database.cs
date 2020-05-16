fca7ded16a8fe56334eb6776ee575eed19aabd9a
LGsus18 <65468322+LGsus18@users.noreply.github.com>
2020-05-16 17:00:32 -0500

    INVENTORY p.1

---
+++ INVENTORY/scripts/Database.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory System/Database")]
+public class Database : ScriptableObject {
+    public List<Item> items = new List<Item>();
+
+    public Item FindItemInDataBase(int id)
+    {
+        foreach(Item item in items)
+        {
+            if(item.id == id)
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+}
+[System.Serializable]
+public class Item
+{
+    public int id;
+    public string name;
+    [TextArea(5,5)]
+    public string description;
+    public int cost;
+    public int sellcost;
+    public int uselevel;
+    public Stats stats;
+
+    [System.Serializable]
+    public struct Stats
+    {
+        public int damage;
+        public int defense;
+    }
+
+}
---
+++ INVENTORY/scripts/Editor/DatabaseEditor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Database))]
+public class DatabaseEditor : Editor {
+
+    private Database database;
+
+    private string searchString;
+
+    private bool shouldSearch;
+
+    private void OnEnable()
+    {
+        database = (Database)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (database)
+        {
+            EditorGUILayout.BeginHorizontal("Box");
+            GUILayout.Label("Items in Database: " + database.items.Count);
+            EditorGUILayout.EndHorizontal();
+
+            foreach (Item item in database.items)
+            {
+                //dibujar la representacion del item
+                DispalyItem(item);
+            }
+        }
+    }   
+    
+    private void DispalyItem(Item item)
+    {
+        GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
+        labelStyle.wordWrap = true;
+
+        EditorGUILayout.BeginVertical("Box");
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("ID: ");
+        GUILayout.Label(item.id.ToString(), labelStyle);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndVertical();
+
+    }
+ }
